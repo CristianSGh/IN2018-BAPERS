b4a1d2937ef8eeb32f5673305d76aa0c54b72446
Cristian Ghita <Cristian.Ghita.CS@gmail.com>
2018-03-07 14:25:14 +0000

    Add BapersMain and EJB
    This should serve as the basis for the server layer of the system

---
+++ BapersMain/src/main/java/uk/ac/city/angelsoftworks18/bapersmain/Main.java
@@ -0,0 +1,16 @@
+package uk.ac.city.angelsoftworks18.bapersmain;
+
+/**
+ * Class containing the "main" method
+ * prints Hello world! by default - replace with your code
+ *
+ */
+
+public class Main {
+    
+    public static void main( String[] args ) {
+        // ...
+        
+    }
+    
+}
