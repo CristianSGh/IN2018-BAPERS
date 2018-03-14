9069baf664c6d7acfa987e4e44c6967a671ac53a
karandixit <karandixit95@googlemail.com>
2018-03-14 19:49:50 +0000

    Working Update

---
+++ Working GUI/BapersGUI/BapersGUI/BapersGUI/src/TeamProject/BapersGUI.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TeamProject;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author cris
+ */
+public class BapersGUI {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame main = new MainWindow();
+        main.setLocationRelativeTo(null);
+        main.setVisible(true);
+     //   JOptionPane.showMessageDialog(main, "Hello.");
+    }
+    
+}
